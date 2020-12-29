38346b086037c7bfb12e0530ddbded9b72af44d3
GNAGONI <nikichetotam@gmail.com>
2020-12-29 14:52:24 -0800

    Implement integration with event bus module, add sockets functionality, implement basic frontend

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 require('express-async-errors');
-const express = require('express');
+const app = require('express')();
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const { eventBus } = require('@microservices-inc/common');
 
-const app = express();
 dotenv.config();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
 app.use((req, res) => {
   res.status(404).send('Not Found');
 });
 
-app.listen(process.env.PRESENCE_SERVICE_PORT, () => {
-  console.log(`Server is running on port ${process.env.PRESENCE_SERVICE_PORT}`);
+io.on('connection', socket => {
+  const interval = setInterval(async () => {
+    const consumer = eventBus.getConsumer('isOnline');
+    if (consumer) {
+      consumer.consume(data => {
+        try {
+          const user = JSON.parse(data.content.toString());
+          socket.emit('isOnline', user);
+        } catch (e) {
+          console.error('Invalid event format');
+        }
+      });
+    }
+  }, 3000);
+
+  socket.on('disconnect', () => {
+    clearInterval(interval);
+  });
+});
+
+eventBus.connect(process.env.RABBITMQ_URI, () => {
+  server.listen(process.env.PRESENCE_SERVICE_PORT, () => {
+    console.log(
+      `Server is running on port ${process.env.PRESENCE_SERVICE_PORT}`,
+    );
+  });
 });
